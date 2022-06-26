305134784029159faf540dcdfeecc5ec2d511a44
Mon120 <baihuagaoyuan@gmail.com>
2022-06-26 16:05:27 +0900

    追加:Spritesを追加

---
+++ Assets/Scripts/ScoreObject_KeyDestroy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreObject_KeyDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Aキーが押された時
+        if (Input.GetKeyDown(KeyCode.A)){
+
+        }
+
+        //Bキーが押された時
+        if (Input.GetKeyDown(KeyCode.B)){
+            
+        }
+
+        //Cキーが押された時
+        if (Input.GetKeyDown(KeyCode.C)){
+            
+        }
+
+        //Dキーが押された時
+        if (Input.GetKeyDown(KeyCode.D)){
+            
+        }
+    }
+}
